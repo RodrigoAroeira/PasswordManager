7491a233fc8ab80f61cebe0fd0cb47ccf2fccf3f
RodrigoAroeira <raroeirabr@gmail.com>
2025-04-14 14:21:43 -0300

    test: add more unit tests for edge cases
    Added additional unit tests for PasswordDatabase to cover edge cases

--- src/storage.rs
+++ src/storage.rs
@@ -44,3 +44,83 @@ impl PasswordDatabase {
         &self.entries
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use tempfile::NamedTempFile;
+
+    use super::*;
+
+    fn get_tmp_file() -> anyhow::Result<(NamedTempFile, String)> {
+        let file = NamedTempFile::new()?;
+        let path = file.path().to_str().unwrap().to_string();
+        Ok((file, path))
+    }
+
+    #[test]
+    fn test_add_and_get_entry() {
+        let (_, path) = get_tmp_file().unwrap();
+        let mut db = PasswordDatabase::new(&path);
+
+        let entry = PasswordEntry {
+            service: String::from("service"),
+            username: String::from("username"),
+            password: String::from("password"),
+        };
+
+        let service = entry.service.clone();
+        db.add(entry);
+        assert!(
+            db.get(&service).is_some(),
+            "Entry was not found in the database"
+        );
+    }
+
+    #[test]
+    fn test_save() {
+        let (_, path) = get_tmp_file().expect("Unable to create tempfile");
+        let mut db = PasswordDatabase::new(&path);
+
+        let entry = PasswordEntry {
+            service: String::from("service"),
+            username: String::from("username"),
+            password: String::from("password"),
+        };
+        let service = entry.service.clone();
+        db.add(entry);
+        assert!(db.save().is_ok());
+        let db2 = PasswordDatabase::new(&path);
+        assert_eq!(db.get(&service), db2.get(&service));
+    }
+
+    #[test]
+    fn test_non_existent_service() {
+        let (_, path) = get_tmp_file().unwrap();
+        let db = PasswordDatabase::new(&path);
+
+        assert!(db.get("Service").is_none(), "Database should be empty");
+    }
+
+    #[test]
+    fn test_empty_database() {
+        let (_, path) = get_tmp_file().unwrap();
+        let db = PasswordDatabase::new(&path);
+
+        assert!(db.entries().is_empty(), "Database should be empty");
+    }
+
+    #[test]
+    fn test_save_and_load_empty_database() {
+        let (_, path) = get_tmp_file().unwrap();
+        let db = PasswordDatabase::new(&path);
+
+        assert!(db.save().is_ok(), "Saving empty database failed");
+
+        let loaded_db = PasswordDatabase::new(&path);
+        assert!(
+            loaded_db.entries().is_empty(),
+            "Loaded database should be empty"
+        );
+    }
+}
